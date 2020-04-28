da06f1be591d0aaa2ac1aff67dad060336b6589a
amosborne <7675373+amosborne@users.noreploy.github.com>
2020-04-27 22:33:11 -0700

    revise Puyo name conflict

--- puyorc.py
+++ puyorc.py
@@ -55,6 +55,12 @@ _FLICKER_CORRELATION_PEAK_PROMINENCE = 0.2
 # 15 frames long. This value is selected somewhat arbitrarily.
 _FLICKER_GROUP_FRAME_WINDOW_SIZE = 15
 
+# Class factories for self-documentation.
+Bean     = namedtuple('Bean', 'row col puyo_type')
+Flicker  = namedtuple('Flicker', 'frameno bean')
+PopGroup = namedtuple('PopGroup', 'frameno beans')
+State    = namedtuple('State', 'frameno board')
+
 def correlateFlicker(raw_puyo, puyo_type):
     """Correlate the puyo classifications to the flicker animation kernel."""
     
@@ -91,7 +97,6 @@ def correlateFlicker(raw_puyo, puyo_type):
 def findFlickers(raw_boards):
     """Find all puyo flickers that meet a correlation threshold."""
 
-    Flicker = namedtuple('Flicker', 'frameno row col puyo_type')
     flickers = []
     for row,col in np.ndindex(raw_boards[0].shape):
         raw_puyo = [board[row,col] for board in raw_boards]
@@ -101,7 +106,7 @@ def findFlickers(raw_boards):
             flicker_corr = correlateFlicker(raw_puyo, puyo_type)
             corr_peaks,_ = find_peaks(flicker_corr, prominence=_FLICKER_CORRELATION_PEAK_PROMINENCE)
             for frameno in corr_peaks:
-                flickers.append(Flicker(frameno,row,col,puyo_type))
+                flickers.append(Flicker(frameno, Bean(row,col,puyo_type)))
     return flickers
 
 def clusterFlickers(flickers):
@@ -116,25 +121,22 @@ def clusterFlickers(flickers):
         else:
             flicker_groups.append([f])
 
-    Puyo = namedtuple('Puyo', 'row col puyo_type')            
-    PopGroup = namedtuple('PopGroup', 'frameno puyos')
     pop_groups = []
     for flicker_group in flicker_groups:
         frameno = flicker_group[0].frameno
         pop_group = set()
         for flicker in flicker_group:
-            puyo = Puyo(flicker.row, flicker.col, flicker.puyo_type)
-            pop_group.add(puyo)
+            pop_group.add(flicker.bean)
         pop_groups.append(PopGroup(frameno, pop_group))
     return pop_groups
 
 def validatePopGroups(pop_groups):
-    """ TODO: Validate pop groups by color and geometry constraints."""
+    """TODO: Validate pop groups by color and geometry constraints."""
     
     return pop_groups
 
 def findPopGroups(raw_boards):
-    """ Find all pop groups (with their respective frames)."""
+    """Find all pop groups (with their respective frames)."""
     
     raw_flickers = findFlickers(raw_boards)
     unvalidated_pop_groups = clusterFlickers(raw_flickers)
@@ -142,7 +144,7 @@ def findPopGroups(raw_boards):
     return validated_pop_groups
 
 def findTransitions(raw_nextpuyo):
-    """ Find all frames where the next puyo pair is drawn."""
+    """Find all frames where the next puyo pair is drawn."""
     
     both_blank = []
     for puyo1,puyo2 in raw_nextpuyo:
@@ -161,21 +163,27 @@ def findTransitions(raw_nextpuyo):
     return transitions
 
 def getPopSequence(prev_trans,next_trans,pop_groups):
+    """Return the list of pop groups that occur between the two transition frames."""
+    
     pop_sequence = []
     for pop_group in pop_groups:
         if prev_trans < pop_group.frameno < next_trans:
             pop_sequence.append(pop_group)
     return pop_sequence
 
-def buildBoardSequence(raw_boards, transitions, pop_groups):
-    """ Build the sequence of boards (with their respective frames)."""
+def getPuyoMajority(raw_puyo):
+    puyo_count = Counter(raw_puyo)
+    return puyo_count.most_common(1).pop(0)
 
-    State = namedtuple('State', 'frameno board')
+def buildBoardSequence(raw_boards, transitions, pop_groups):
+    """Build the sequence of boards (with their respective frames)."""
+
     # Initial the first board in the list of board states to be empty.
     initial_board = np.empty_like(raw_boards[0], dtype=Puyo)
     initial_board.fill(Puyo.NONE)
     board_states = [State(0, initial_board)]
     for idx,this_trans in enumerate(transitions):
+        current_board_state = board_states
         # Get the indices of the previous and next transitions.
         if idx == 0:
             prev_trans = 0
@@ -189,7 +197,6 @@ def buildBoardSequence(raw_boards, transitions, pop_groups):
         # Get whether the previous and next frame windows had pops.
         prev_pop_sequence = getPopSequence(prev_trans, this_trans, pop_groups)
         next_pop_sequence = getPopSequence(this_trans, next_trans, pop_groups)
-        print(next_pop_sequence)
         if not prev_pop_sequence:
             pass # determine previous puyo placement by -3,+4 majority window about transition
         if not next_pop_sequence:
@@ -202,14 +209,14 @@ import puyodebug
 import cv2
 
 def robustClassify(raw_clf):
-    """ Return the board state sequence. Raw classifications are assumed to begin after
-        the first puyo pair is drawn and ends at an arbitrary time (but not long after
-        game loss). Final puyo pair placement during a fatal blow will not be captured.
+    """Return the board state sequence. Raw classifications are assumed to begin after
+       the first puyo pair is drawn and ends at an arbitrary time (but not long after
+       game loss). Final puyo pair placement during a fatal blow will not be captured.
     
-        Raw classifications are received as a list of tuples:
-           * Index is the frame number.
-           * First element is a numpy array representing the board. (board[row][col] = puyo)
-           * Second element is a tuple of the next puyo pair.
+       Raw classifications are received as a list of tuples:
+          * Index is the frame number.
+          * First element is a numpy array representing the board. (board[row][col] = puyo)
+          * Second element is a tuple of the next puyo pair.
     """
     
     raw_boards, raw_nextpuyo = tuple(zip(*raw_clf))
