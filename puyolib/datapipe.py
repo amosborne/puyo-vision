7e607986b0a6747aa69ccbd81c1f3906264a413b
amosborne <7675373+amosborne@users.noreploy.github.com>
2020-05-04 21:13:15 -0700

    threaded multiprocessing

--- puyolib/datapipe.py
+++ puyolib/datapipe.py
@@ -1,8 +1,9 @@
 import multiprocessing as mp
+import threading
 import subprocess
 import os
 import cv2
-import time
+from collections import deque as Deque
 from puyolib.vision import processFrame
 
 import timeit
@@ -11,28 +12,20 @@ VIDEO_FILE = "./dev/momoken_vs_tom2.mp4"
 VIDEO_CHUNK_DEST = ".tmp/"
 TEMP_FILE_PREFIX = "tmp"
 
+CPU_COUNT = 5
 
-def processVideo(src_filepath, start_time, end_time):
+
+def processVideo(src, start, end):
     """Return... something?"""
 
-    # For the designated amount of time, chunk the video for each spare processor.
-    no_cpu = os.cpu_count()
-    _, f_ext = os.path.splitext(VIDEO_FILE)
-    video_chunk_filepaths = []
-    for idx in range(0, no_cpu - 1):
-        tmp_file = os.path.join(VIDEO_CHUNK_DEST, TEMP_FILE_PREFIX + str(idx) + f_ext)
-        start = round(idx * end_time / (no_cpu - 1))
-        end = round(start + (end_time / (no_cpu - 1)))
-        chunkVideo(src_filepath, tmp_file, start, end)
-        video_chunk_filepaths.append(tmp_file)
+    video_chunk_filepaths = makeVideoChunks(src, CPU_COUNT, start, end)
 
     # Spawn the parallel processors and output queue.
     frame_processors = []
-    data_queue = mp.Queue(maxsize=256)
+    data_queue = mp.Queue()
     for path in video_chunk_filepaths:
-        args = (path, data_queue)
         frame_processors.append(
-            mp.Process(target=parallelProcessVideo, args=args, daemon=True)
+            mp.Process(target=processChunk, args=(path, data_queue), daemon=True)
         )
     for proc in frame_processors:
         proc.start()
@@ -45,13 +38,13 @@ def processVideo(src_filepath, start_time, end_time):
         if data_queue.empty():
             continue
         result = data_queue.get()
-        if result is None:
+        if result is not None:
+            data_compiled.append(result)
+        else:
             sentinel_count += 1
             if sentinel_count == len(frame_processors):
                 queue_complete = True
                 data_queue.close()
-        else:
-            data_compiled.append(result)
 
     for proc in frame_processors:
         proc.join()
@@ -60,42 +53,67 @@ def processVideo(src_filepath, start_time, end_time):
     return None  # ?
 
 
-def parallelProcessVideo(src_filepath, data_queue):
-    """Place into the data queue the processed results of each frame."""
+def readFrames(src, deque):
+    """Read video frames into a deque."""
 
-    cap = cv2.VideoCapture(src_filepath)
+    cap = cv2.VideoCapture(src)
     while True:
         ret, frame = cap.read()
-        if not ret:
+        if ret:
+            deque.appendleft(frame)
+        else:
             break
-        result = processFrame(frame)
-        data_queue.put(result)
-    # Release and return None as a sentinel.
     cap.release()
+    deque.appendleft(None)
+
+
+def processChunk(src, data_queue):
+    """Return the processed results of each frame in the video chunk."""
+
+    frame_deque = Deque()
+    frame_thread = threading.Thread(
+        target=readFrames, args=(src, frame_deque), daemon=True
+    )
+    frame_thread.start()
+
+    while True:
+        if frame_deque:
+            frame = frame_deque.pop()
+            if frame is not None:
+                result = processFrame(frame)
+                data_queue.put(result)
+            else:
+                break
     data_queue.put(None)
+    frame_thread.join()
 
 
-def chunkVideo(src_filepath, dest_filepath, start_time, end_time):
-    """ Chunk the input video into a copy, units seconds."""
+def chunkVideo(src, dest, start, end):
+    """Chunk the input video into a copy (with FFMPEG), units seconds."""
 
-    cmd = (
-        "ffmpeg -y -i "
-        + src_filepath
-        + " -ss "
-        + str(start_time)
-        + " -to "
-        + str(end_time)
-        + " "
-        + dest_filepath
-    )
-    subp = subprocess.run(
-        cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-    )
+    start, end = tuple(map(str, (start, end)))
+    args = ["ffmpeg", "-y", "-i", src, "-ss", start, "-to", end, dest]
+    subp = subprocess.run(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return subp.returncode
 
 
+def makeVideoChunks(src, n, start, end):
+    """Return temporary filepaths to chunked videos."""
+
+    _, f_ext = os.path.splitext(src)
+    video_chunks = []
+    for idx in range(n):
+        tmp_file = TEMP_FILE_PREFIX + str(idx) + f_ext
+        tmp_file = os.path.join(VIDEO_CHUNK_DEST, tmp_file)
+        cstart = round(idx * end / n)
+        cend = round(cstart + (end / n))
+        chunkVideo(src, tmp_file, cstart, cend)
+        video_chunks.append(tmp_file)
+    return video_chunks
+
+
 def main():
-    t = timeit.Timer(lambda: processVideo(VIDEO_FILE, 0, 60))
+    t = timeit.Timer(lambda: processVideo(VIDEO_FILE, 0, 20))
     print(t.timeit(number=1))
 
 
